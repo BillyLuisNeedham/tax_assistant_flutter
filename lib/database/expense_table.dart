9cb05f753fc3821e01a574c1e7fae046f6b6ddff
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-11 11:15:29 +0100

    expense table now has create table static method

--- lib/database/expense_table.dart
+++ lib/database/expense_table.dart
@@ -1,3 +1,5 @@
+import 'package:sqflite/sqflite.dart';
+
 class ExpenseTable {
   static final tableName = 'expense_table';
   static final columnId = '_id';
@@ -6,4 +8,17 @@ class ExpenseTable {
   static final columnAmount = '_amount';
   static final columnReceiptUrl = '_receipt_url';
   static final columnNote = '_columnNote';
+
+  static Future onCreateExpenseTable(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE ${ExpenseTable.tableName} (
+          ${ExpenseTable.columnId} INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          ${ExpenseTable.columnName} TEXT NOT NULL,
+          ${ExpenseTable.columnDateTimeStamp} TEXT NOT NULL ,
+          ${ExpenseTable.columnAmount} REAL NOT NULL,
+          ${ExpenseTable.columnReceiptUrl} TEXT NOT NULL,
+          ${ExpenseTable.columnNote} TEXT NOT NULL
+        )
+        ''');
+  }
 }
