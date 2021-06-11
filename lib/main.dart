594f3d194a3f192e1592c6298f822ae1ebe6514c
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-06-11 11:15:44 +0100

    main dart now inits sqlite

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:tax_assistant_flutter/database/sqlite.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final db = SqliteDB();
+  await db.countTable();
+
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -20,17 +25,9 @@ void main() {
 }
 
 
+
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
